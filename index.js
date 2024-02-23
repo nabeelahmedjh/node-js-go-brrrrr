7acc437a4ab12b72236e550563740564b4622474
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2024-02-23 11:56:28 +0500

    node hihihi

---
+++ index.js
@@ -0,0 +1,18 @@
+// import express from 'express'
+
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/:name', (req, res) => {
+    res.send('Hello, ' + req.params.name);
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
